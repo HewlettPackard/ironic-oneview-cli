5154147483a79fbfb0b88acc801e16c902d0a2e6
Xavier <marcusrafael@lsd.ufcg.edu.br>
2016-03-14 17:28:49 -0300

    generate rc file

--- ironic_oneview_cli/genconfig/commands.py
+++ ironic_oneview_cli/genconfig/commands.py
@@ -23,87 +23,77 @@ from builtins import input
 from configparser import ConfigParser
 
 
+def generate_rc_file(file_name, **credentials):
+    with open(file_name, 'w') as rc:
+        for key in credentials.keys():
+            value = credentials[key]
+            line = "export %s=%s" % (key, value)
+            rc.write(line + '\n')
+            rc.truncate()
+
+
 def do_genconfig(args):
     """Generates the config file according to user input
 
     """
-    print("========= Ironic ========= ")
-    ironic_auth_url = input("Type the auth_url for the Ironic service: ")
-    ironic_username = input("Type your Ironic username: ")
-    ironic_tenant = input("Type your Ironic user's tenant name: ")
-    ironic_password = getpass.getpass("Type your Ironic user's password: ")
-    ironic_insecure = input("Would you like the connections with Ironic to be"
-                            " insecure? [y/N]: ") or "N"
-    ironic_insecure = 'True' if ironic_insecure.lower() == 'y' else 'False'
-    ironic_ca_file = ""
-    if not ironic_insecure:
-        ironic_ca_file = input("Type the path to the Ironic ca_file: ")
-    default_deploy_kernel = input("Type in the default deploy kernel image ID"
-                                  " on Glance: ")
-    default_deploy_ramdisk = input("Type in the default deploy ramdisk image"
-                                   " ID on Glance: ")
 
-    # TODO(thiagop): get drivers enabled with ironicclient
     enabled_drivers = ['agent_pxe_oneview', 'iscsi_pxe_oneview']
-    ironic_default_driver = input("Which driver would you like to use? [%s]: "
-                                  % ','.join(enabled_drivers))
+    openstack_cacert = ''
+    oneview_cacert = ''
 
-    option = input("Would you like to configure different credentials for"
-                   " nova? [y/N]: ")
-    option = True if option.lower() == 'y' else False
-    if option:
-        print("========= Nova ========= ")
-        nova_auth_url = input("Type the auth_url for the Nova service: ")
-        nova_username = input("Type your Nova username: ")
-        nova_tenant = input("Type your Nova user's tenant name: ")
-        nova_password = getpass.getpass("Type your Nova user's password: ")
-        nova_insecure = input("Would you like the connections with Nova to be"
-                              " insecure? [y/N]: ") or "N"
-        nova_insecure = 'True' if nova_insecure.lower() == 'y' else 'False'
-    else:
-        nova_auth_url = ironic_auth_url
-        nova_username = ironic_username
-        nova_tenant = ironic_tenant
-        nova_password = ironic_password
-        nova_insecure = ironic_insecure
 
-    print("========= OneView ========= ")
-    oneview_manager_url = input("Type in the OneView url: ")
+    # OpenStack
+
+    openstack_auth_url = input("Type the auth_url for the OpenStack service: ")
+    openstack_username = input("Type your OpenStack username: ")
+    openstack_tenant = input("Type your OpenStack user's tenant name: ")
+    openstack_password = getpass.getpass("Type your Openstack user's password: ")
+    openstack_insecure = input("Would you like to allow insecure connections "
+                               "to OpenStack? [y/N]: ") or "N"
+    openstack_ironic_deploy_kernel_uuid = input("Type in the default deploy "
+                                                "keynel image ID on Glance: ")
+    openstack_ironic_deploy_ramdisk_uuid = input("Type in the default deploy "
+                                                 "ramdisk image ID on Glance: ")
+    openstack_ironic_node_driver = input(("Which driver would you like to use? "
+                                          "[%s]: ") % ','.join(enabled_drivers))
+    openstack_insecure = 'True' if openstack_insecure.lower() == 'y' else 'False'
+
+    if not openstack_insecure:
+        openstack_cacert = input("Type the path to the OpenStack ca_file: ")
+
+
+    # OneView
+
+    oneview_manager_url = input("Type in the OneView uri: ")
     oneview_username = input("Type your OneView username: ")
     oneview_password = getpass.getpass("Type your OneView user's password: ")
-    allow_insecure = input("Would you like the connections with OneView to be"
-                           " insecure? [y/N]: ") or "N"
-    allow_insecure = 'True' if allow_insecure.lower() == 'y' else 'False'
+    oneview_insecure = input("Would you like to allow insecure connections "
+                             "to OneView? [y/N]: ") or "N"
+    oneview_insecure = 'True' if oneview_insecure.lower() == 'y' else 'False'
 
-    config = ConfigParser()
-    config.add_section("ironic")
-    config.set("ironic", "auth_url", ironic_auth_url)
-    config.set("ironic", "admin_user", ironic_username)
-    config.set("ironic", "admin_tenant_name", ironic_tenant)
-    config.set("ironic", "admin_password", ironic_password)
-    config.set("ironic", "insecure", ironic_insecure)
-    config.set("ironic", "default_deploy_kernel_id", default_deploy_kernel)
-    config.set("ironic", "default_deploy_ramdisk_id", default_deploy_ramdisk)
-    config.set("ironic", "default_driver", ironic_default_driver)
-    config.set("ironic", "ca_file", ironic_ca_file)
-    config.add_section("nova")
-    config.set("nova", "auth_url", nova_auth_url)
-    config.set("nova", "username", nova_username)
-    config.set("nova", "tenant_name", nova_tenant)
-    config.set("nova", "password", nova_password)
-    config.set("nova", "insecure", nova_insecure)
-    config.add_section("oneview")
-    config.set("oneview", "manager_url", oneview_manager_url)
-    config.set("oneview", "username", oneview_username)
-    config.set("oneview", "password", oneview_password)
-    config.set("oneview", "allow_insecure_connections", allow_insecure)
+    if not oneview_insecure:
+        oneview_cacert = input("Type the path to the OneView ca_file: ")
 
     filename = input("Type the path to the new configuration file [%s]: "
                      % args.config_file) or args.config_file
-    full_filename = os.path.realpath(os.path.expanduser(filename))
-    directory = os.path.dirname(full_filename)
+    real_path = os.path.expanduser(filename)
+    fullname = os.path.realpath(os.path.expanduser(filename))
+    directory = os.path.dirname(fullname)
     if not os.path.exists(directory):
         os.makedirs(directory)
-    with open(full_filename, 'w') as configfile:
-        config.write(configfile)
-        print("======\nFile created successfully on '%s'!\n======" % filename)
+
+    credentials = dict()
+    credentials['OS_USERNAME'] = openstack_username
+    credentials['OS_PASSWORD'] = openstack_password
+    credentials['OS_PROJECT_NAME'] = openstack_tenant
+    credentials['OS_TENANT_NAME'] = credentials['OS_PROJECT_NAME']
+    credentials['OS_CACERT'] = openstack_cacert
+    credentials['OS_AUTH_URL'] = openstack_auth_url
+    credentials['OS_IRONIC_DEPLOY_KERNEL_UUID'] = openstack_ironic_deploy_kernel_uuid
+    credentials['OS_IRONIC_DEPLOY_RAMDISK_UUID'] = openstack_ironic_deploy_ramdisk_uuid
+    credentials['OS_IRONIC_NODE_DRIVER'] = openstack_ironic_node_driver
+    credentials['OV_URL'] = oneview_manager_url
+    credentials['OV_USERNAME'] = oneview_username
+    credentials['OV_PASSWORD'] = oneview_password
+    credentials['OV_CACERT'] = oneview_cacert
+    generate_rc_file(args.config_file, **credentials)
