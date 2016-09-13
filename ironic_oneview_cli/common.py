06c3bae5793d4407334e385d421853b33a2282d0
Hugo Nicodemos <nicodemos@lsd.ufcg.edu.br>
2016-09-13 14:32:54 +0000

    Update common.py rights

--- ironic_oneview_cli/common.py
+++ ironic_oneview_cli/common.py
@@ -1,6 +1,7 @@
 # Copyright 2015 Hewlett-Packard Development Company, L.P.
 # Copyright 2015 Universidade Federal de Campina Grande
 # Copyright 2012 Red Hat, Inc.
+# All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
