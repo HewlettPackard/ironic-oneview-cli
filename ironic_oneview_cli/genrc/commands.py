876999b07ee227da71e973c7dfdece4d08b76267
Hugo Nicodemos <nicodemos@lsd.ufcg.edu.br>
2016-11-07 17:29:42 +0000

    Change genrc command to generate a sample script file.

--- ironic_oneview_cli/genrc/commands.py
+++ ironic_oneview_cli/genrc/commands.py
@@ -14,96 +14,54 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-
-from builtins import input
-from ironic_oneview_cli import common
-
-
-def _generate(**data):
-    bash_file = ''
-
-    echo = 'echo "Please enter your HP OneView password: "'
-    read = 'read -sr OV_PASSWORD_INPUT'
-    export = 'export OV_PASSWORD=$OV_PASSWORD_INPUT'
-
-    for key in data.keys():
-        value = data[key]
-        line = "export %s=%s" % (key, value)
-        bash_file = bash_file + line + '\n'
-
-    return bash_file + echo + '\n' + read + '\n' + export + '\n'
-
-
-def _save(path, filename='ironic-oneviewrc.sh', **data):
-    if filename in path:
-        path = path.replace(filename, '')
-
-    canonical = os.path.realpath(os.path.expanduser(path))
-    directory = os.path.dirname(canonical)
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    full_path = os.path.join(canonical, filename)
-    bash = _generate(**data)
-
-    with open(full_path, 'w') as ironic_oneviewrc:
-        for line in bash:
-            ironic_oneviewrc.write(line)
-
-    return full_path
-
 
 def do_genrc(args):
-    """Generates the ironic-oneviewrc.sh file according to user input."""
+    """Generates the Ironic OneView CLI sample file."""
+
+    print("#!/usr/bin/env bash")
+    print("\n# Ironic OneView CLI")
 
     # OneView
 
-    auth_url = input("HP OneView URL: ")
-    username = input("HP OneView username: ")
-    cacert = input(
-        "HP OneView cacert file path (only for secure connections): ")
-    polling_attempts = input(
-        "Max connection retries to check changes on OneView [Default=12]: ")
-    audit_enabled = input("Enable OneView Audit (y/N): ")
-    audit_enabled = True if audit_enabled.lower() == 'y' else False
+    print("\n# OneView HPE Authentication URL.")
+    print("#export OV_AUTH_URL=")
 
-    if audit_enabled:
-        audit_map_file = input("OneView Audit input file absolute path: ")
-        audit_output_file = input("OneView Audit output file absolute path: ")
+    print("\n# OneView HPE username.")
+    print("#export OV_USERNAME=")
+
+    print("\n# Path to OneView CA certificate bundle file."
+          " (only for secure connmections)")
+    print("#export OV_CACERT=")
+
+    print("\n# Max connection retries to check change on OneView HPE.")
+    print("#export OV_MAX_POLLING_ATTEMPTS=12")
+
+    print("\n# Enable OneView Audit Logging.")
+    print("#export OV_AUDIT=False")
+
+    print("\n# Path to map file for OneView audit cases. \n"
+          "# Used only when OneView API audit is enabled. \n"
+          "# See: https://github.com/openstack/python-oneviewclient")
+    print("#export OV_AUDIT_INPUT=")
+
+    print("\n# Path to OneView audit output file. (json format) \n"
+          "# Created only when Oneview API audit is enabled.")
+    print("#export OV_AUDIT_OUTPUT=")
+
+    print("\n# Pass the OneView HPE password when executed.")
+    print("echo \"Please enter your HP OneView password: \"")
+    print("read -sr OV_PASSWORD_INPUT")
+    print("export OV_PASSWORD=$OV_PASSWORD_INPUT")
 
     # OpenStack
 
-    ironic_deploy_kernel = input("Ironic deploy kernel image uuid: ")
-    ironic_deploy_ramdisk = input("Ironic deploy ramdisk image uuid: ")
-    ironic_driver = input(
-        "Select an Ironic OneView driver [%s]"
-        "\nDefault to agent_pxe_oneview: " %
-        ', '.join(common.SUPPORTED_DRIVERS)
-    )
-    if ironic_driver not in common.SUPPORTED_DRIVERS:
-        ironic_driver = 'agent_pxe_oneview'
+    print("\n# Ironic deploy kernel image uuid.")
+    print("#export OS_IRONIC_DEPLOY_KERNEL_UUID=")
 
-    path = input("Path to save 'ironic-oneviewrc.sh' file [%s]: " %
-                 os.path.join(os.getcwd(), 'ironic-oneviewrc.sh'))
+    print("\n# Ironic deploy ramdisk image uuid.")
+    print("#export OS_IRONIC_DEPLOY_RAMDISK_UUID=")
 
-    # Map Informations
-
-    ironic_oneviewrc = dict()
-    ironic_oneviewrc['OV_AUTH_URL'] = auth_url
-    ironic_oneviewrc['OV_USERNAME'] = username
-    ironic_oneviewrc['OV_CACERT'] = cacert
-    ironic_oneviewrc['OV_MAX_POLLING_ATTEMPTS'] = polling_attempts
-    ironic_oneviewrc['OS_IRONIC_DEPLOY_KERNEL_UUID'] = ironic_deploy_kernel
-    ironic_oneviewrc['OS_IRONIC_DEPLOY_RAMDISK_UUID'] = ironic_deploy_ramdisk
-    ironic_oneviewrc['OS_IRONIC_NODE_DRIVER'] = ironic_driver
-    ironic_oneviewrc['OV_AUDIT'] = audit_enabled
-
-    if audit_enabled:
-        ironic_oneviewrc['OV_AUDIT_INPUT'] = audit_map_file
-        ironic_oneviewrc['OV_AUDIT_OUTPUT'] = audit_output_file
-
-    # Create ironic-oneviewrc.sh
-
-    print('File saved in %s' % (_save(path, **ironic_oneviewrc)))
+    print(
+        "\n# OneView Ironic driver for node creation. \n"
+        "# Allowed values: agent_pxe_oneview, iscsi_pxe_oneview, fake_oneview")
+    print("#export OS_IRONIC_NODE_DRIVER=agent_pxe_oneview")
